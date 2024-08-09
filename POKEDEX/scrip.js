ec402eea063f63b28a1c8041e5c2635cee2bf8c0
Joao-Germano <jaogermano076@gmail.com>
2024-08-09 16:54:55 -0300

    Add files via upload

---
+++ POKEDEX/scrip.js
@@ -0,0 +1,30 @@
+ function buscar(){
+    var entrada = document.getElementById("entrada").value.toLowerCase()
+
+    var url = "https://pokeapi.co/api/v2/pokemon/"+entrada+ "";
+   
+
+    fetch(url).then(response => response.json()).then(data =>{
+        var tela = document.getElementById('tela')
+
+        tela .innerHTML =
+
+        '<h2>' + data.name + '<h2>'
+
+        +' <img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'+data.id+'.gif"; > '
+        
+        +' <img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/'+data.id+'.gif"; > '
+
+        + '<p> Id: ' + data.id + '</p>'
+        + '<p> Tipo: ' + data.types.map(type => type.type.name) + '</p>'
+
+        + '<p> Habilidades: '+ data.abilities.map(abilities => abilities.ability.name) + '</p>'
+        
+
+        document.getElementById("entrada").value = '';
+
+    }).catch(erro => {
+        var tela = document.getElementById('tela');
+        tela.innerHTML = '<p>Pokemóm não encontrado, erro: ' + erro
+    })
+ }
